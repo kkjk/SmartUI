63bf12dfeadcbcbc4dd160dee64fae74d5ea3329
SmartcitiesIOT <smartiotss19@gmail.com>
2019-06-22 17:51:43 +0200

    Protobuf read C# in WPF App

--- SmartUI/MainWindow.xaml.cs
+++ SmartUI/MainWindow.xaml.cs
@@ -12,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using SensorMsg;
+using Google.Protobuf;
 
 namespace SmartUI
 {
@@ -23,6 +27,55 @@ namespace SmartUI
         public MainWindow()
         {
             InitializeComponent();
+        }
+       
+        private void MqttStart(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("MqttStarted");
+            status.Text = "Connected";
+            Sensor.Text = "False";
+            MqttClient myClient = new MqttClient("192.168.0.103");
+
+            // Register to message received
+            myClient.MqttMsgPublishReceived += client_recievedMessage;
+
+            string clientId = Guid.NewGuid().ToString();
+            myClient.Connect(clientId);
+
+            // Subscribe to topic
+            myClient.Subscribe(new String[] { "base" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+            System.Console.ReadLine();
+        }
+         void client_recievedMessage(object sender, MqttMsgPublishEventArgs e)
+        {
+            // Handle message received
+            System.Console.WriteLine("Message received: " + e.Message);
+            SensorData NewData;
+            // Watch the protbuf magic here
+            NewData = SensorData.Parser.ParseFrom(e.Message);
+            this.Dispatcher.Invoke(() =>
+            {
+                UpdateValues(NewData);
+                
+            });
+            
+            System.Console.WriteLine("Message received: " + NewData.BatteryLevel);
+            System.Console.WriteLine("Message received: " + NewData.Luminance);
+            System.Console.WriteLine("Message received: " + NewData.RelativeHumidity);
+            System.Console.WriteLine("Message received: " + NewData.Temperature);
+            System.Console.WriteLine("Message received: " + NewData.Motion);
+            System.Console.WriteLine("Message received: " + NewData.Ultraviolet);
+
+        }
+        public void UpdateValues(SensorData NewData)
+        {
+            Sensor.Text = NewData.Motion.ToString();
+            UV.Text = NewData.Ultraviolet.ToString();
+            Temperature.Text = NewData.Temperature.ToString();
+            Humidity.Text = NewData.RelativeHumidity.ToString();
+            Luminance.Text = NewData.Luminance.ToString();
+            battery.Text = NewData.BatteryLevel.ToString();
+            //Add Time using google protobuf wellknown tyes
         }
     }
 }
